747fa8d26a0a7770edc1ae2640fc73d055601539
alyson albasi <aly@mcjerz.net>
2022-09-19 12:24:13 -0400

    .

--- src/main/java/com/aa/bookish/controllers/BookController.java
+++ src/main/java/com/aa/bookish/controllers/BookController.java
@@ -1,5 +1,37 @@
 package com.aa.bookish.controllers;
 
+import com.aa.bookish.dtos.BookDTO;
+import com.aa.bookish.models.BookModel;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
 public class BookController {
 
+//    move to service class
+    private final Map<Integer, BookModel> books = new HashMap<>();
+
+
+    @PostMapping("/")
+    public BookDTO create(
+//            @RequestHeader("authorization") final String pAuthn,
+            @RequestBody final BookDTO pDTO)
+    {
+        final BookModel model = pDTO.toModel();
+        books.put(model.getId(), model);
+
+        return BookDTO.of(model);
+    }
+
+    @GetMapping("/")
+    public Collection<BookDTO> getAll() {
+        return books.values().stream()
+                .map(BookDTO::of)
+                .collect(Collectors.toList());
+    }
 }
---
+++ src/main/java/com/aa/bookish/services/BookService.java
@@ -0,0 +1,11 @@
+package com.aa.bookish.services;
+
+import com.aa.bookish.models.BookModel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BookService {
+    //move to service class
+    private final Map<Integer, BookModel> books = new HashMap<>();
+}
